1b0026fa645bf99456b2a293218aeb0ecf4539d0
Diego Alexis Estrada Gonzalez <codeEsgo29@gmail.com>
2023-01-21 15:15:24 -0600

    Este es el segundo commit del sabado 21

--- IDGS803-python-intro/Practica2.py
+++ IDGS803-python-intro/Practica2.py
@@ -1,36 +1,43 @@
-def pedir(num1, num2):
-    num1 = int(input("Ingresa el primer numero:"))
-    num1 = int(input("Ingresa el primer numero:"))
-
-
 def sumar(num1,num2):
-    suma = num1 + num2
-    print("El resultado de la suma es: {}".format(suma))
-    main()
+    print("{} + {} = {}".format(num1, num2, num1 + num2))
 
 def restar(num1,num2):
-    resta = num1 - num2
-    print("El resultado de la suma es: {}".format(resta))
-    main()
+    print("{} - {} = {}".format(num1, num2, num1 - num2))
 
 def multiplicar(num1,num2):
-    mult = num1 * num2
-    print("El resultado de la suma es: {}".format(mult))
-    main()
+    print("{} x {} = {}".format(num1, num2, num1 * num2))
 
 def dividir(num1,num2):
-    resta = num1 / num2
-    print("El resultado de la suma es: {}".format(resta))
-    main()
+    print("{} / {} = {}".format(num1, num2, num1 / num2))
 
-def salir():
-    print("Gracias por usar la")
 
 def main():
-    print("Elige la opción del menú: \n 1.-Sumar \n 2.-Restar \n 3.-Multiplicar \n 4.-Dividir \n 5.-Salir")
+    print("----BIENVENIDO A EL MENÚ---- \n 1.-Sumar \n 2.-Restar \n 3.-Multiplicar \n 4.-Dividir \n 5.-Salir")
 
 if __name__ == "__main__":
     main()
+
+opcion = int(input("Ingrese una opcion: "))
+
+num1 = int(input("Ingresa el primer numero:"))
+num2 = int(input("Ingresa el primer numero:"))
+
+while opcion != 5:
+    if opcion == 1:
+      sumar(num1,num2)
+    elif opcion == 2:
+      restar(num1,num2)
+    elif opcion == 3:
+      multiplicar(num1,num2)
+    elif opcion == 4:
+      dividir(num1,num2)
+    else:
+       print("--- La opción elegida no se encuetra dentro del menú ---")
+
+    main()
+    opcion = int(input("Ingrese una opcion: "))
+
+print("!!!Gracias por utilizar mi sistema!!!")
 
 
 
